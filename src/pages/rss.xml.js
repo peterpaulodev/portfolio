0e414aaae6a1d8bf885539b794f2e327832f25bd
Peterson Paulo <peterpaulodev@gmail.com>
2025-10-22 15:58:56 -0300

    chore: adjust rss xml infos

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,10 +2,10 @@ import rss, { pagesGlobToRssItems } from '@astrojs/rss';
 
 export async function GET(context) {
   return rss({
-    title: 'Web Development and Technology Blog | Fernando López | EFEELE',
-    description: 'Welcome to my blog, where I share my passion for frontend development, web design, and the latest technology trends.',
+    title: 'Peterson Almeida | Portfólio',
+    description: 'Um template moderno e minimalista criado para desenvolvedores e criativos digitais.',
     site: context.site,
     items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
-    customData: `<language>es</language>`,
+    customData: `<language>pt</language>`,
   });
 }
