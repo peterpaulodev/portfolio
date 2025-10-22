0a217b9d1741b7d7688ddb0f32d3941b2b202105
Peterson Paulo <peterpaulodev@gmail.com>
2025-10-22 16:01:11 -0300

    chore: astro config add site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://neonmint.efeele.dev",
+  site: "https://peterdev-portfolio.vercel.app/",
   integrations: [preact(), icon(), sitemap({
     filter: (page) =>
       !page.includes("/blog/tags") &&
